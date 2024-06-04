919605d5645e656c9102a0402620d3b3e61eb6ce
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-04 14:33:06 +0200

    feat: construct, get and set of attributes in product.php

--- Models/product.php
+++ Models/product.php
@@ -7,4 +7,42 @@ class Product {
     private float $price;
     private string $image;
     public Category $category;
+
+    public function __construct($name, $price) {
+        $this->name = $name;
+        
+        $this->price = $price; 
+    }
+
+    
+    
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * Get the value of price
+     */ 
+    public function getPrice(): float {
+        return $this->price;
+    }
+
+    /**
+     * Get the value of image
+     */ 
+    public function getImage(): string {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     * @return  self
+     */ 
+    public function setImage(string $image): void {
+        $this->image = $image;
+    }
 }
